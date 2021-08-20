41a69b5c393dabe22cf33102ffd4e7df2bf893cf
Marco Starker <marco.starker@gmx.net>
2021-08-20 18:34:17 +0200

    feat: parsing for exports

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 'use strict';
 
+const searchFor = [
+    'exports.',
+    'exports[\'default\']'
+];
+
+/**
+ *
+ * @param {string} source
+ * @returns {number}
+ */
+function findPos(source) {
+    for (const value of searchFor) {
+        const pos = source.indexOf(value);
+        if (pos !== -1) {
+            return pos;
+        }
+    }
+
+    return -1;
+}
+
 module.exports = exports = function () {
     return {
         renderChunk: function (source) {
             const matches = /(exports(?:\['default']|\.default)) = (.*);/gi.exec(source);
-            if (matches) {
-                const pos = source.indexOf('exports.');
-
-                source = source.substring(0, pos) + `module.exports = exports = ${matches[2]};\n\n` + source.substring(pos);
+            if (matches === null || matches.length < 3) {
+                return source;
             }
-            return source;
+
+            const pos = findPos(source);
+            if (pos === -1) {
+                return source;
+            }
+
+            return source.substring(0, pos) + `module.exports = exports = ${matches[2]};\n\n` + source.substring(pos);
         }
     };
 }
